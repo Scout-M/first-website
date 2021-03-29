cc3b19b65e1e5e14796b8b12fbc12347e7008ce2
Amy Mangor <aemangor@gmail.com>
2021-03-29 11:22:33 -0400

    added folders, changed class,id,image names

---
+++ javascript/script.js
@@ -0,0 +1,7 @@
+function setNewImage() {
+  document.getElementById("G-Fish").src = "images/shark.jpg";
+}
+
+function setOldImage() {
+  document.getElementById("G-Fish").src = "images/gFish.png";
+}
