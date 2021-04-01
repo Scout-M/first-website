f43f9df6e305dd4b3dbec721cb1a1c221d8cc99a
Amy Mangor <aemangor@gmail.com>
2021-04-01 10:24:34 -0400

    add images and javascript

--- javascript/script.js
+++ javascript/script.js
@@ -1,7 +1,26 @@
+document.addEventListener("DOMContentLoaded", function () {
+  imageChange();
+});
+
 function setNewImage() {
   document.getElementById("G-Fish").src = "images/shark.jpg";
 }
 
 function setOldImage() {
   document.getElementById("G-Fish").src = "images/gFish.png";
+}
+
+function imageChange() {
+  const images = document.querySelectorAll(".myFace");
+  let i = 0;
+  setInterval(function () {
+    images.forEach((img, i) => {
+      img.style.display = "none";
+    });
+    if (i == images.length) {
+      i = 0;
+    }
+    images[i].style.display = "block";
+    i++;
+  }, 3000);
 }
